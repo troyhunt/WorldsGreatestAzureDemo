02390e8b545d82ef3683ac80d0a19ae07bcb89f7
Troy Hunt <troyhunt@hotmail.com>
2014-02-19 18:26:35 +1100

    Got rid of more cra... uh, I mean stuff I don't need

--- WorldsGreatestAzureDemo/Scripts/_references.js
+++ WorldsGreatestAzureDemo/Scripts/_references.js
@@ -1,5 +1,4 @@
 ﻿/// <autosync enabled="true" />
 /// <reference path="jquery-2.1.0.js" />
 /// <reference path="bootstrap.js" />
-/// <reference path="respond.js" />
 /// <reference path="grayscale.js" />
