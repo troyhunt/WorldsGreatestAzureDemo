84234959b490fe60fb37a6189dd11a3910b95a41
Troy Hunt <troyhunt@hotmail.com>
2014-02-19 18:13:13 +1100

    Added a fairly basic templated site

---
+++ WorldsGreatestAzureDemo/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace WorldsGreatestAzureDemo
+{
+  public class FilterConfig
+  {
+    public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+    {
+      filters.Add(new HandleErrorAttribute());
+    }
+  }
+}
---
+++ WorldsGreatestAzureDemo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WorldsGreatestAzureDemo.MvcApplication" Language="C#" %>
