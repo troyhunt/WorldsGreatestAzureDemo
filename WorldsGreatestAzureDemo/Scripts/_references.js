9fc6c9dfad100ebc5b2d926b9698063c322edad1
Troy Hunt <troyhunt@hotmail.com>
2014-03-13 22:50:46 +1100

    Updated NuGet packages

--- WorldsGreatestAzureDemo/Scripts/_references.js
+++ WorldsGreatestAzureDemo/Scripts/_references.js
@@ -1,4 +1,4 @@
 ﻿/// <autosync enabled="true" />
 /// <reference path="jquery-2.1.0.js" />
-/// <reference path="bootstrap.js" />
 /// <reference path="grayscale.js" />
+/// <reference path="bootstrap.js" />
