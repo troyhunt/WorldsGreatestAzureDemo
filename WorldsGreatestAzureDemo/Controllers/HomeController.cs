3a8b72aebf10198573ad959125a0f86e5ad14bbf
Troy Hunt <troyhunt@hotmail.com>
2017-03-02 16:31:01 +1000

    Removed cookies used for test

--- WorldsGreatestAzureDemo/Controllers/HomeController.cs
+++ WorldsGreatestAzureDemo/Controllers/HomeController.cs
@@ -1,5 +1,4 @@
-﻿using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace WorldsGreatestAzureDemo.Controllers
 {
@@ -7,20 +6,6 @@ namespace WorldsGreatestAzureDemo.Controllers
   {
     public ActionResult Index()
     {
-      var secureCookie = new HttpCookie("SecureCookie")
-      {
-        Secure = true,
-        Value = "This cookie is secure!"
-      };
-      Response.Cookies.Add(secureCookie);
-
-      var insecureCookie = new HttpCookie("InsecureCookie")
-      {
-        Secure = false,
-        Value = "This cookie is not secure!"
-      };
-      Response.Cookies.Add(insecureCookie);
-
       return View();
     }
   }
